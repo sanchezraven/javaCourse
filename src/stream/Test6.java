4ed67f91c592562c7b47c4cb7574ed655c93ff10
alex_voronin <sanchez_raven@mail.ru>
2023-12-13 22:01:23 +0500

    passed peek

--- src/stream/Test6.java
+++ src/stream/Test6.java
@@ -8,5 +8,13 @@ public class Test6 {
         Stream<Integer> stream2 = Stream.of(6,7,8,9, 10);
         Stream<Integer> stream3 = Stream.concat(stream1, stream2);
         stream3.forEach(System.out::println);
+        System.out.println("______________________");
+
+        Stream<Integer> stream5 = Stream.of(1,2,3,4,5, 1, 2, 3);
+        //stream5.distinct().forEach(System.out::println);
+
+        System.out.println("________________");
+        //System.out.println(stream5.count());
+        System.out.println(stream5.distinct().peek(System.out::println).count());
     }
 }
