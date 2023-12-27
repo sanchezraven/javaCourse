b489203ae523a63313e5f9d5e6a83ce7ff922518
alex_voronin <sanchez_raven@mail.ru>
2023-12-27 20:30:16 +0500

    passed volatible

---
+++ src/multithreading/VolatileEx.java
@@ -0,0 +1,23 @@
+package src.multithreading;
+
+public class VolatileEx extends Thread{
+    volatile boolean b = true;
+
+    public void run() {
+        long counter = 0;
+        while (b) {
+            counter++;
+        }
+        System.out.println("loop is finished, counter = " + counter);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        VolatileEx thread = new VolatileEx();
+        thread.start();
+        Thread.sleep(3000);
+        System.out.println("After 3 seconds it is time to wake up");
+        thread.b = false;
+        thread.join();
+        System.out.println("End of programm");
+    }
+}
